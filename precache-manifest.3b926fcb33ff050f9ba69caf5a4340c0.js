683d917ec6b4f0bfbf313bab305ae57fca1891d8
Siddarth Kumar <siddarthkay@gmail.com>
2020-05-10 14:54:41 +0530

    deploy to githubpages

---
+++ precache-manifest.3b926fcb33ff050f9ba69caf5a4340c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826a0c451644994838c453ecd6838006",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1643e2ea4e6ece407b82",
+    "url": "/static/css/main.a000dcce.chunk.css"
+  },
+  {
+    "revision": "ef63d3237181a4076d8a",
+    "url": "/static/js/2.75d58725.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75d58725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1643e2ea4e6ece407b82",
+    "url": "/static/js/main.0f6f4802.chunk.js"
+  },
+  {
+    "revision": "54544d211bb412302c81",
+    "url": "/static/js/runtime-main.23afc54a.js"
+  }
+]);
